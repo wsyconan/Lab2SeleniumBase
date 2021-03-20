ed301b2a67e8496e35628fd8f7fa1a29fddbb4c1
wsyconan <wsyconan@gmail.com>
2021-03-20 16:31:18 +0300

    Add comments.

--- src/test/java/hw2/ex1.java
+++ src/test/java/hw2/ex1.java
@@ -19,60 +19,59 @@ import java.util.Locale;
 public class ex1 {
     WebDriver webDriver;
     WebElement webElement;
-    String url, str, mainTitle, jdiText, subHeader, jdiUrl;
+    String url, str, mainTitle, jdiText, subHeader, jdiUrl, username, password;
     int i;
     List<WebElement> list;
     String[] headers, textUnderPic;
 
     @Test
     public void test() {
-        // Step 1
+        // Step 1: Open test site by URL
         webDriver.get(url);
 
-        // Step 2
+        // Step 2: Assert Browser title
         Assert.assertEquals(webDriver.getTitle(), "Home Page");
+        // maximize window
         webDriver.manage().window().maximize();
 
-        // Step 3
+        // Step 3: Perform login
         webDriver.findElement((By.cssSelector("li.dropdown.uui-profile-menu"))).click();
-        webDriver.findElement(By.cssSelector("#name")).sendKeys("Roman");
-        webDriver.findElement(By.cssSelector("#password")).sendKeys("Jdi1234");
+        webDriver.findElement(By.cssSelector("#name")).sendKeys(username);
+        webDriver.findElement(By.cssSelector("#password")).sendKeys(password);
         webDriver.findElement(By.cssSelector("#login-button")).click();
 
-        // Step 4
+        // Step 4: Assert User name in the left-top side of screen that user is loggined
         String userName = webDriver.findElement(By.cssSelector("#user-name")).getText();
         Assert.assertEquals(userName, "ROMAN IOVLEV");
 
-        // Step 5
+        // Step 5: Assert Browser title
         Assert.assertEquals(webDriver.getTitle(), "Home Page");
 
-        // Step 6
+        // Step 6: Assert that there are 4 items on the header section are displayed and they have proper texts
         list = webDriver.findElements(By.cssSelector("ul.uui-navigation.nav.navbar-nav.m-l8 a"));
         i = 0;
         for (WebElement we : list) {
             if (we.isDisplayed() && we.isEnabled()) {
-                Assert.assertEquals(headers[i], we.getText());
-                i++;
+                Assert.assertEquals(headers[i++], we.getText());
             }
         }
 
-        // Step 7
+        // Step 7: Assert that there are 4 images on the Index Page and they are displayed
         list = webDriver.findElements(By.cssSelector(".benefit-icon"));
         Assert.assertEquals(list.size(), 4);
         for (WebElement we : list) {
             Assert.assertTrue(we.isDisplayed());
         }
 
-        // Step 8
+        // Step 8: Assert that there are 4 texts on the Index Page under icons and they have proper text
         list = webDriver.findElements(By.cssSelector(".benefit-icon+.benefit-txt"));
         Assert.assertEquals(list.size(), 4);
         i = 0;
         for (WebElement we : list) {
-            Assert.assertEquals(we.getText(), textUnderPic[i]);
-            i++;
+            Assert.assertEquals(textUnderPic[i++], we.getText());
         }
 
-        // Step 9
+        // Step 9: Assert a text of the main headers
         webElement = webDriver.findElement(By.cssSelector("h3.main-title.text-center"));
         Assert.assertTrue(webElement.isDisplayed());
         str = webElement.getText();
@@ -83,39 +82,40 @@ public class ex1 {
         str = webElement.getText();
         Assert.assertEquals(str, jdiText.toUpperCase(Locale.ROOT));
 
-        // Step 10
+        // Step 10: Assert that there is the iframe in the center of page
         list = webDriver.findElements(By.cssSelector("iframe"));
         Assert.assertFalse(list.isEmpty());
 
-        // Step 11
+        // Step 11: Switch to the iframe
         webDriver.switchTo().frame("second_frame");
+        // Check that there is Epam logo in the left top conner of iframe
         webElement = webDriver.findElement(By.cssSelector("img#epam-logo"));
         Assert.assertTrue(webElement.isDisplayed());
 
-        // Step 12
+        // Step 12: Switch to original window back
         webDriver.switchTo().defaultContent();
 
-        // Step 13
+        // Step 13: Assert a text of the sub header
         webElement = webDriver.findElement(By.cssSelector("h3.text-center>a"));
         Assert.assertTrue(webElement.isDisplayed());
         str = webElement.getText();
         Assert.assertEquals(str, subHeader);
 
-        // Step 14
+        // Step 14: Assert that JDI GITHUB is a link and has a proper URL
         webElement = webDriver.findElement(By.cssSelector("h3.text-center>a[target=_blank]"));
         Assert.assertTrue(webElement.isDisplayed());
         str = webElement.getAttribute("href");
         Assert.assertEquals(str, jdiUrl);
 
-        // Step 15
+        // Step 15: Assert that there is Left Section
         webElement = webDriver.findElement(By.cssSelector("ul.sidebar-menu"));
         Assert.assertTrue(webElement.isDisplayed());
 
-        // Step 16
+        // Step 16: Assert that there is Footer
         webElement = webDriver.findElement(By.cssSelector("footer"));
         Assert.assertTrue(webElement.isDisplayed());
 
-        // Step 17
+        // Step 17: Close Browser
         webDriver.close();
 
         // Delay
@@ -133,6 +133,8 @@ public class ex1 {
         //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         webDriver = new ChromeDriver();
         url = "https://jdi-testing.github.io/jdi-light/index.html";
+        username = ("Roman");
+        password = ("Jdi1234");
         headers = new String[]{"HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"};
         textUnderPic = new String[]{"To include good practices\n" + "and ideas from successful\n" + "EPAM project",
                 "To be flexible and\n" + "customizable",
@@ -140,14 +142,14 @@ public class ex1 {
                 "Already have good base\n" + "(about 20 internal and\n" +
                         "some external projects),\n" + "wish to get more…"
         };
-        mainTitle = new String("EPAM FRAMEWORK WISHES…");
-        jdiText = new String("LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR " +
+        mainTitle = ("EPAM FRAMEWORK WISHES…");
+        jdiText = ("LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR " +
                 "INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION " +
                 "ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN " +
                 "REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR."
         );
-        subHeader = new String("JDI GITHUB");
-        jdiUrl = new String("https://github.com/epam/JDI");
+        subHeader = ("JDI GITHUB");
+        jdiUrl = ("https://github.com/epam/JDI");
     }
 
     @AfterTest
--- src/test/java/hw2/ex2.java
+++ src/test/java/hw2/ex2.java
@@ -20,74 +20,79 @@ public class ex2 {
     WebDriver webDriver;
     WebElement webElement;
     SoftAssert softAssert;
-    String url, str;
+    String url, username, password;
     String[] serviceOptionsTop, serviceOptionsSide;
     List<WebElement> list;
     int i;
     Pattern patternWater, patternWind, patternSelen, patternYellow, patternWaterFalse, patternWindFalse;
-    Matcher matcher;
 
     @Test
     public void test() {
-        // Step 1
+        // Step 1: Open test site by URL
         webDriver.get(url);
 
-        // Step 2
+        // Step 2: Assert Browser title
         softAssert.assertEquals(webDriver.getTitle(), "Home Page");
         webDriver.manage().window().maximize();
 
-        // Step 3
+        // Step 3: Perform login
         webDriver.findElement((By.cssSelector("li.dropdown.uui-profile-menu"))).click();
-        webDriver.findElement(By.cssSelector("#name")).sendKeys("Roman");
-        webDriver.findElement(By.cssSelector("#password")).sendKeys("Jdi1234");
+        webDriver.findElement(By.cssSelector("#name")).sendKeys(username);
+        webDriver.findElement(By.cssSelector("#password")).sendKeys(password);
         webDriver.findElement(By.cssSelector("#login-button")).click();
 
-        // Step 4
+        // Step 4: Assert User name in the left-top side of screen that user is loggined
         String userName = webDriver.findElement(By.cssSelector("#user-name")).getText();
         softAssert.assertEquals(userName, "ROMAN IOVLEV");
 
-        // Step 5
+        // Step 5: Click on "Service" subcategory in the header
         webDriver.findElement(By.cssSelector("li.dropdown>a.dropdown-toggle")).click();
         list = webDriver.findElements(By.cssSelector("ul.dropdown-menu>li>a"));
         Assert.assertFalse(list.isEmpty());
+        // Check the drop down contains options
         i = 0;
         for (WebElement we : list) {
             softAssert.assertEquals(we.getText(), serviceOptionsTop[i++].toUpperCase(Locale.ROOT));
         }
 
-        // Step 6
+        // Step 6: Click on Service subcategory in the left section
         webDriver.findElement(By.cssSelector("li.menu-title[index='3']")).click();
         list = webDriver.findElements(By.cssSelector("li.menu-title[index='3'] li[index]>a"));
         Assert.assertFalse(list.isEmpty(), "Step 6 list was empty.");
+        // Check that drop down contains options
         i = 0;
         for (WebElement we : list) {
             softAssert.assertEquals(we.getText(), serviceOptionsSide[i++]);
         }
 
-        // Step 7
+        // Step 7: Open through the header menu "Service" -> "Different elements" Page
         webDriver.findElement(By.cssSelector("ul.sub>li[index='8']")).click();
 
-        // Step 8
+        // Step 8: Check interface on Different elements page
+        // 4 checkboxes
         list = webDriver.findElements(By.cssSelector("input[type='checkbox']"));
         softAssert.assertEquals(list.size(), 4);
+        // 4 radios
         list = webDriver.findElements(By.cssSelector("input[type='radio']"));
         softAssert.assertEquals(list.size(), 4);
+        // 1 dropdown
         list = webDriver.findElements(By.cssSelector("select.uui-form-element"));
         softAssert.assertEquals(list.size(), 1);
+        // 2 buttons
         webElement = webDriver.findElement(By.cssSelector("button.uui-button"));
         softAssert.assertTrue(webElement.isEnabled());
         webElement = webDriver.findElement(By.cssSelector("input.uui-button"));
         softAssert.assertTrue(webElement.isEnabled());
 
-        // Step 9
+        // Step 9: Assert that there is Right Section
         webElement = webDriver.findElement(By.cssSelector("#mCSB_2"));
         softAssert.assertTrue(webElement.isDisplayed());
 
-        // Step 10
+        // Step 10: Assert that there is Left Section
         webElement = webDriver.findElement(By.cssSelector("#mCSB_1"));
         softAssert.assertTrue(webElement.isDisplayed());
 
-        // Step 11
+        // Step 11: Select checkboxes "Water" and "Wind"
         list = webDriver.findElements(By.cssSelector("label.label-checkbox"));
         Assert.assertFalse(list.isEmpty());
         for (WebElement we : list) {
@@ -96,13 +101,14 @@ public class ex2 {
             }
         }
 
-        // Step 12
+        // Step 12: Assert that for each checkbox there is an individual log row
+        //         and value is corresponded to the status of checkbox.
         list = webDriver.findElements(By.cssSelector("ul.panel-body-list.logs li"));
         Assert.assertFalse(list.isEmpty());
         softAssert.assertTrue(patternWind.matcher(list.get(0).getText()).matches());
         softAssert.assertTrue(patternWater.matcher(list.get(1).getText()).matches());
 
-        // Step 13
+        // Step 13: Select radio "Selen"
         list = webDriver.findElements(By.cssSelector("label.label-radio"));
         Assert.assertFalse(list.isEmpty());
         for (WebElement we : list) {
@@ -111,12 +117,13 @@ public class ex2 {
             }
         }
 
-        // Step 14
+        // Step 14: Assert that for radiobutton there is a log row
+        //         and value is corresponded to the status of radiobutton.
         list = webDriver.findElements(By.cssSelector("ul.panel-body-list.logs li"));
         Assert.assertFalse(list.isEmpty());
         softAssert.assertTrue(patternSelen.matcher(list.get(0).getText()).matches());
 
-        // Step 15
+        // Step 15: Select option "Yellow" in dropdown
         webDriver.findElement(By.cssSelector("select.uui-form-element")).click();
         list = webDriver.findElements(By.cssSelector("select.uui-form-element>option"));
         for (WebElement we : list) {
@@ -125,12 +132,13 @@ public class ex2 {
             }
         }
 
-        // Step 16
+        // Step 16: Assert that for dropdown there is a log row
+        //         and value is corresponded to the selected value.
         list = webDriver.findElements(By.cssSelector("ul.panel-body-list.logs li"));
         Assert.assertFalse(list.isEmpty());
         softAssert.assertTrue(patternYellow.matcher(list.get(0).getText()).matches());
 
-        // Step 17
+        // Step 17: Unselect and assert checkboxes "Water" and "Wind"
         list = webDriver.findElements(By.cssSelector("label.label-checkbox"));
         Assert.assertFalse(list.isEmpty());
         for (WebElement we : list) {
@@ -139,7 +147,8 @@ public class ex2 {
             }
         }
 
-        // Step 18
+        // Step 18: Assert that for each checkbox there is an individual log row
+        //         and value is corresponded to the status of checkbox.
         list = webDriver.findElements(By.cssSelector("ul.panel-body-list.logs li"));
         Assert.assertFalse(list.isEmpty());
         softAssert.assertTrue(patternWindFalse.matcher(list.get(0).getText()).matches());
@@ -159,6 +168,8 @@ public class ex2 {
     public void beforeTest() {
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
+        username = ("Roman");
+        password = ("Jdi1234");
         softAssert = new SoftAssert();
         url = "https://jdi-testing.github.io/jdi-light/index.html";
         serviceOptionsSide = new String[]{
